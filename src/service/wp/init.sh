df2ba8d59eba8e628e05c15b2745848ae236e2da
Juyeong Maing <mjy9088@naver.com>
2023-10-26 14:14:27 +0900

    complete

--- src/service/wp/init.sh
+++ src/service/wp/init.sh
@@ -5,4 +5,4 @@ set -e
 [ -d /app/wordpress ] || (rm -rf /root/wordpress && wp-cli.phar core download --path=/root/wordpress && mv /root/wordpress /app/wordpress)
 
 [ -f /app/wordpress/wp-config.php ] || wp-cli.phar config create --dbhost=db --dbname=${MARIADB_DATABASE} --dbuser=${MARIADB_USER} --dbpass=${MARIADB_PASSWORD} --path=/app/wordpress
-wp-cli.phar core install --url=${INTRA_LOGIN}.42.fr --title="${WORDPRESS_TITLE}" --admin_name="${WORDPRESS_USER}" --admin_password="${WORDPRESS_PASSWORD}" --admin_email=${WORDPRESS_EMAIL} --path=/app/wordpress
+wp-cli.phar core install --url=${INTRA_LOGIN}.42.fr --title="${WORDPRESS_TITLE}" --admin_name="${WORDPRESS_USER}" --admin_password="${WORDPRESS_PASSWORD}" --admin_email=${WORDPRESS_EMAIL} --path=/app/wordpress --url="https://${INTRA_LOGIN}.42.fr/wordpress"
